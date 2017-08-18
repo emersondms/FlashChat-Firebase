2498d16f1b082eea48957ef9a383eecb637de0f9
John Doe <john@sample.com>
2017-08-18 15:58:33 -0300

    First commit

---
+++ app/src/androidTest/java/com/london_app_brewery/flashchatnewfirebase/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.london_app_brewery.flashchatnewfirebase;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/com/london_app_brewery/flashchatnewfirebase/InstantMessage.java
@@ -0,0 +1,22 @@
+package com.london_app_brewery.flashchatnewfirebase;
+
+public class InstantMessage {
+
+    private String message, author;
+
+    public InstantMessage(String message, String author) {
+        this.message = message;
+        this.author = author;
+    }
+
+    public InstantMessage() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
